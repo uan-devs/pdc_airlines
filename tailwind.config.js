b25f18a74ace160acef2e77fd80683f1fed7dcc4
Eliúde Vemba <sweeteliude@gmail.com>
2023-01-11 06:15:50 +0100

    Set new HomePage

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.jsx',
+        './resources/js/**/*.js',
+    ],
+
+    theme: {},
+    corePlugins: {
+        preflight: false,
+    },
+
+    plugins: [],
+};
